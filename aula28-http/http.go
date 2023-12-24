2a20ab0e9a5d6820c32443f295e2de10b766699a
leandrosouzasnts <1eandrosouzasnts@gmail.com>
2023-12-24 12:24:50 -0300

    Manipulando html em GO

--- aula28-http/http.go
+++ aula28-http/http.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Seja bem-vindo!"))
+	templates.ExecuteTemplate(w, "home.html", nil)
 }
 
 func users(w http.ResponseWriter, r *http.Request) {
@@ -18,8 +19,11 @@ func rotas() {
 	http.HandleFunc("/users", users)
 }
 
+var templates *template.Template
+
 func main() {
 
+	templates = template.Must(template.ParseGlob("*.html"))
 	rotas()
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
